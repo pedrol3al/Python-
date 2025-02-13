368937a420b07df5b1b1189424aa45725f5129dc
pedrol3al <pedrogabrieleal@gmail.com>
2025-02-13 15:42:18 -0300

    Teste dois

--- Atividades/Adjacente.py
+++ Atividades/Adjacente.py
@@ -11,6 +11,6 @@ numero = int(input("Digite o número: "))
 
 # Verificação e saída
 if tem_adjacentes_iguais(numero): #Teste github
-    print("Sim")
+    print("Sim") #Teste github 2
 else:
     print("Não")
